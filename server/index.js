a4de5e86dcfcb587fde628905db103d5a3f7e968
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-02-25 02:17:50 -0500

    Trying to make sure .env doesn't get pushed

--- server/index.js
+++ server/index.js
@@ -2,9 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const mainRouter = require("./routes/mainRouter");
 const PORT = process.env.PORT;
+const cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["https://127.0.0.1:5173", "https://127.0.0.1:5174"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/v1", mainRouter);
 
 app.listen(PORT, (err) => {
